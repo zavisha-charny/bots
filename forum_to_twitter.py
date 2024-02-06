23ec601dcec654905dd31045bcf89bb5d2de54ac
zavisha-charny <158227621+zavisha-charny@users.noreply.github.com>
2024-02-06 19:01:15 +0000

    Add files via upload

---
+++ forum_to_twitter.py
@@ -0,0 +1,268 @@
+import os
+import re
+import enum
+import json
+import logging
+from datetime import datetime
+from dataclasses import dataclass
+
+import httpx
+import numpy as np
+from dotenv import load_dotenv
+from more_itertools import flatten
+from better_profanity import profanity
+from selectolax.parser import HTMLParser
+
+
+logger = logging.getLogger()
+load_dotenv()
+profanity.load_censor_words()
+
+
+def remove_html(html: str) -> str:
+    tree = HTMLParser(html)
+    return tree.text().replace('\n', ' ')
+
+
+def profanity_only(text: str) -> bool:
+    text = re.sub(r'@\w+', '', text)
+    text = re.sub(r'[^\w]', '', text)
+    return len(text) == 0
+
+
+class Periods(enum.Enum):
+    YESTERDAY = 86400 
+    ONE_WEEK = 604800
+    TWO_WEEKS = 1209600
+    ONE_MONTH = 2592000
+    THREE_MONTHS = 7776000
+    SIX_MONTHS = 15552000
+    ONE_YEAR = 31104000
+
+
+class Categories(enum.Enum):
+    BIOENERGETICS = 5
+    CASE_STUDIES = 6
+    NOOSPHERE = 7
+    JUNKYARD = 8
+    META = 9
+
+
+@dataclass
+class Post:
+    uid: int
+    username: str
+    is_banned: bool
+    replies_to: str | None
+    date: datetime
+    content: str
+    text: str | None = None
+
+    def __post_init__(self) -> None:
+        self.username = profanity.censor(self.username)
+        if profanity_only(self.username):
+            self.username = str(self.uid)
+        if self.replies_to is not None:
+            self.replies_to = profanity.censor(self.replies_to)
+        self.text = profanity.censor(remove_html(self.content))
+        self.content = profanity.censor(self.content)
+
+    def to_tweet(self) -> str:
+        date = self.date.strftime('%Y-%m-%d')
+        tweet = f'[{date}] {self.username}'
+        if self.replies_to is not None:
+            tweet += f' to {self.replies_to}'
+        tweet += ': '
+        tweet += self.text
+        return tweet
+
+
+@dataclass 
+class Topic:
+    id: int
+    title: str
+    author: str
+    author_uid: int
+    date: datetime
+    posts: list[Post]
+
+    def __post_init__(self) -> None:
+        self.title = profanity.censor(self.title)
+        self.author = profanity.censor(self.author)
+        if profanity_only(self.author):
+            self.author = str(self.author_uid)
+
+
+CATEGORY_BLACKLIST = [Categories.JUNKYARD]
+PERIOD = Periods.ONE_MONTH
+BASE_URL = 'https://bioenergetic.forum/api/'
+BOT_USERNAME = 'brad'
+IS_TWITTER_PREMIUM = False
+TWITTER_CHAR_LIMIT = 4_000 if IS_TWITTER_PREMIUM else 280
+HEADERS = {'Content-Type': 'application/json'}
+CREDENTIALS = {
+  'username': os.environ['FORUM_USERNAME'],
+  'password': os.environ['FORUM_PASSWORD'],
+}
+
+def load_auth_cookie() -> str:
+    return os.environ['AUTH']
+
+
+def get_recent_mentions(page: int = None) -> dict:
+    """
+    Searches for the mentions of the bot newer than PERIOD.
+
+    The relevant keys in the response are: 
+
+    'posts'         : list of posts on the first page
+    'matchCount'    : number of all posts
+    'pageCount'     : number of pages
+    'pagination'    : dict with pages' information
+    'multiplePages' : boolean value indicating if result is paginated
+    """
+    url = BASE_URL + f'search?in=posts&term=%40{BOT_USERNAME}&matchWords=all&by=&categories=&searchChildren=false&hasTags=&replies=&repliesFilter=atleast&timeFilter=newer&timeRange={PERIOD.value}&sortBy=timestamp&sortDirection=desc&showAs=posts'
+    if page is not None:
+        url += f'&page={page}'
+    try:
+
+        with httpx.Client() as client:
+            # create session
+            client.post(
+                BASE_URL+'v3/utilities/login', 
+                headers=HEADERS,
+                data=CREDENTIALS
+            )
+            response = client.get(url)
+            response.raise_for_status()
+            data = response.json()
+        logger.info(f'Mentions {"" if page is None else "Page "+str(page)+" "}OK: {response.status_code} ({data["time"]}s).')
+    except httpx.HTTPError as e:
+        logger.error(f'Mentions {"" if page is None else "Page "+str(page)+" "}ERROR: {response.status_code}.')
+        logger.critical(f'HTTP Exception for {e.request.url}: {e}.')
+        raise e
+
+    return data
+
+
+def extract_topics(mentions: dict) -> list[str]:
+    """
+    Returns unique topic slugs that are not in CATEGORY_BLACKLIST 
+    for all mentions provided.
+    """
+    if mentions['multiplePages']:
+        topics = []
+        for page in range(2, mentions['pageCount']+1):
+            next_mentions = get_recent_mentions(page)
+            topics += [post['topic'] for post in next_mentions['posts']]
+    else:
+        topics = [post['topic'] for post in mentions['posts']]
+
+    topic_slugs = [topic['slug'] for topic in topics if topic['cid'] not in CATEGORY_BLACKLIST]
+    return list(set(topic_slugs))
+
+
+def postprocess_posts(posts: list[Post]) -> list[Post]:
+    # remove posts added after taggin the bot
+    tagging_posts = np.where([f'@{BOT_USERNAME}' in post.content for post in posts])[0]
+    if len(tagging_posts) == 0:
+        # temporary testing fix
+        tagging_posts = [999]
+    posts = [post for i, post in enumerate(posts) if i not in tagging_posts and i < max(tagging_posts)]
+    # remove posts by banned users 
+    posts = [post for post in posts if not post.is_banned]
+    # remove posts containing only profanity
+    posts = [post for post in posts if not profanity_only(post.text)]
+    return posts
+
+
+def compile_posts_into_topic(topic_slug: str) -> Topic:
+    """
+    Given topic slug, get all posts in the topic.
+    """
+    def get_page(url: str, page_num: int) -> dict:
+        try:
+            with httpx.Client() as client:
+                # create session
+                client.post(
+                    BASE_URL+'v3/utilities/login', 
+                    headers=HEADERS,
+                    data=CREDENTIALS
+                )
+                response = client.get(url)
+                response.raise_for_status()
+                data = response.json()
+            logger.info(f'Topic {topic_slug} Page {page_num} OK: {response.status_code}.')
+        except httpx.HTTPError as e:
+            logger.error(f'Topic {topic_slug} Page {page_num} ERROR: {response.status_code}.')
+            logger.critical(f'HTTP Exception for {e.request.url}: {e}.')
+            raise e
+        return data
+    
+
+    def extract_posts(page: dict) -> list[Post]:
+        return [
+            Post(
+                uid=post['uid'],
+                username=post['user']['username'],
+                is_banned=post['user']['banned'],
+                replies_to=post.get('parent', {}).get('username'),
+                date=datetime.strptime(page['timestampISO'], '%Y-%m-%dT%H:%M:%S.%fZ'),
+                content=post['content'],
+            ) for post in page['posts']
+        ]
+
+    # get the first page
+    url = BASE_URL+'topic/'+topic_slug
+    page = get_page(url, 1) 
+    page_count = page['pagination']['pageCount']
+    id = page['tid']
+    title = page['title']
+    author = page['author']['username']
+    author_uid = page['author']['uid']
+    date = datetime.strptime(page['timestampISO'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    posts = extract_posts(page)
+
+    for i in range(1, page_count+1):
+        page = get_page(url+f'?page={i}', i)
+        posts += extract_posts(page)
+
+    posts = postprocess_posts(posts)
+
+    return Topic(id, title, author, author_uid, date, posts)
+
+
+def split_text_on_words(text: str, char_limit: int = TWITTER_CHAR_LIMIT) -> list[str]:
+    if len(text) <= char_limit:
+        return [text.strip(' \n')]
+    
+    lines = [line.strip(' \n') for line in re.findall(r'.{%d}' % char_limit, text)]
+
+    ending_idx = text.rfind(lines[-1])+len(lines[-1])
+    maybe_last_line = text[ending_idx:].strip(' \n')
+    if maybe_last_line:
+        lines.append(maybe_last_line)
+
+    return lines
+
+
+def topic_to_thread(topic: Topic) -> list[str]:
+    date = topic.date.strftime('%Y-%m-%d')
+    title = f'{topic.title} by {topic.author} ({date})'
+    title_tweets = split_text_on_words(title)
+
+    post_tweets = list(flatten([split_text_on_words(post.to_tweet()) for post in topic.posts]))
+    return title_tweets + post_tweets
+
+
+if __name__ == '__main__':
+    mentions = get_recent_mentions()
+    topic_slugs = extract_topics(mentions)
+    threads = []
+    for topic_slug in topic_slugs:
+        topic = compile_posts_into_topic(topic_slug)
+        thread = topic_to_thread(topic)
+        threads.append(thread)
+        print(json.dumps(thread, indent=2))
+        break
+
---
+++ twitter_to_forum.py
@@ -0,0 +1,180 @@
+import os
+import time
+import logging
+from datetime import datetime
+
+import httpx
+import tweepy
+import pandas as pd
+from dotenv import load_dotenv
+
+
+logger = logging.getLogger()
+load_dotenv()
+
+
+IDS_FILE = './extant_tweet_ids.csv'
+TWITTER_BOT_USERNAME = '@mybot'
+CID = -1  # create new category for Twitter re-posts
+BASE_URL = 'https://bioenergetics.forum/api/v3/'
+HEADERS = {'Content-Type': 'application/json'}
+CREDENTIALS = {
+  'username': os.environ['FORUM_USERNAME'],
+  'password': os.environ['FORUM_PASSWORD'],
+}
+
+
+class Tweet:
+    DATE_FORMAT = '%Y-%m-%d %H:%M'
+
+    def __init__(self, status_dict: dict) -> None:
+        self.id = status_dict['id']
+        self.user = status_dict['user']['screen_name']
+        self.user_tag = status_dict['user']['name']
+        self.text = status_dict.get('full_text', status_dict.get('text'))
+        if self.text is None:
+            raise ValueError(f'text can not be None: {status_dict = }')
+        self.created_at = datetime.strptime(status_dict['created_at'], '%a %b %d %H:%M:%S %z %Y')
+        self.in_reply_to_status_id = status_dict['in_reply_to_status_id']
+        self.in_reply_to_user_id = status_dict['in_reply_to_user_id']
+        self.in_reply_to_screen_name = status_dict['in_reply_to_screen_name']
+        self.is_quote = status_dict['is_quote_status']
+
+        self.media = status_dict['entities'].get('media')
+
+    def __str__(self) -> str:
+        header = f'[{self.created_at.strftime(Tweet.DATE_FORMAT)}] (@{self.user_tag}) {self.user}'
+        if self.in_reply_to_screen_name is not None: 
+            header += f' to {self.in_reply_to_screen_name}'
+        if self.media:
+            img_urls = [m['media_url_https'] for m in self.media]
+            footer = '\n' + ' '.join([f'[{os.path.basename(url)}]({url})' for url in img_urls])
+        else:
+            footer = ''
+        return f'{header}:\n{self.text}{footer}'
+
+
+def post_forum(posts: list[dict]) -> bool:
+    posts = [Tweet(post) for post in posts]
+    with httpx.Client() as client:
+        # create session
+        client.post(
+            BASE_URL+'utilities/login', 
+            headers=HEADERS,
+            data=CREDENTIALS
+        )
+        # create a new topic
+        first_post = str(posts[0])
+        title = first_post[:70]
+        if len(first_post) < 70:
+            title += '...'
+        create_payload = {
+            'cid': CID,
+            'title': title,
+            'content': first_post,
+            'timestamp': time.time_ns(),
+            'tags': [],  # some NLP to extract keywords?
+        }
+        response = client.post(
+            BASE_URL+'topics/',
+            headers=HEADERS,
+            data=create_payload,
+        )
+        response.raise_for_status()  # how to handle errors?
+        tid = response.json()['response']['tid']
+        # post all other tweets
+        for post in posts[1:]:
+            post = str(post)
+            post_payload = {
+                'content': post,
+                'toPid': 0,  # ???
+            }
+            response = client.post(
+                BASE_URL+f'topics/{tid}',
+                headers=HEADERS,
+                data=post_payload,
+            )
+            response.raise_for_status()
+
+
+
+def unroll_thread(api: tweepy.API, comment_status: dict) -> tuple[str, str, list]:
+    """
+    Scraps tweets from the start of the thread to the tagging comment. 
+    Returns the thread display name, username, and the list of tweets.
+    """
+    thread_id = comment_status.in_reply_to_status_id
+    start_of_thread = api.get_status(thread_id)
+    author_username = start_of_thread.username
+
+    replies = []
+    for reply in tweepy.Cursor(
+        api.search_tweets, 
+        q=f'to:@{author_username}', 
+        tweet_mode='extended',
+    ).items():
+        if hasattr(reply, 'in_reply_to_status_id'):
+            if (reply.in_reply_to_status_id == thread_id):
+                replies.append(reply)
+    replies = replies[::-1]
+
+    return replies
+
+
+class MyStreamListener(tweepy.StreamingClient):
+
+    def on_tweet(self, tweet: dict) -> None:
+        if not self.__is_valid_comment(tweet):
+            return
+        author_display, author_username, tweets = unroll_thread(tweet.id_str)
+        post_forum(author_display, author_username, tweets)
+        self.__save_tweet_id(tweet['id'])
+        self.__on_success(tweet)
+
+    def __on_success(self, tweet: dict) -> None:
+        # save to csv
+        with open(IDS_FILE, 'a') as f:
+            f.write(str(tweet['id'])+',\n')
+        # respond
+        self.__respond_on_success(tweet)
+
+    def __respond_on_success(self, tweet: dict) -> None:
+        pass
+
+    def __is_valid_comment(self, tweet: dict) -> bool:
+        id_ = tweet['id']
+        df = pd.read_csv(IDS_FILE, names=['id'])
+        return df.loc[df['id'] == id_].empty
+
+    def __save_tweet_id(self, tweet_id: int) -> None:
+        with open(IDS_FILE, 'a') as f:
+            f.write(f'{tweet_id},\n')
+
+
+
+def main() -> None:
+    if not os.path.exists(IDS_FILE):
+        with open(IDS_FILE, 'w') as _:
+            pass
+
+    CONSUMER_KEY = os.environ['API_KEY']
+    CONSUMER_SECRET = os.environ['API_KEY_SECRET']
+    BEARER_TOKEN = os.environ['BEARER_TOKEN']
+    ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+    ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+
+    # authenticate to Twitter
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    # create API object
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    # create a stream listener
+    api = object
+    my_stream_listener = MyStreamListener()
+    my_stream = tweepy.Stream(auth=api.auth, listener=my_stream_listener)
+
+    # start the stream to listen for mentions
+    my_stream.filter(track=[TWITTER_BOT_USERNAME])
+
